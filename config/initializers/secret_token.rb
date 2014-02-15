17d581534f1dfcd590c0ef8989ec3c383f413c6c
Steven Carter <stonemason@gmail.com>
2014-02-15 14:00:23 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoveLetters::Application.config.secret_key_base = '4dbef6dc9c5f8e7248d9f3fea24f3603f4f67adbd1b2dd69302e7cd5c3807fdd3df28c9c5a9f218dc7fcebfecaf894204c037bf6c7814a2ddd04c8c559595f99'
