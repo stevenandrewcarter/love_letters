339dccf280fc15cf18e1b87f736764ae984c9108
Steven Carter <stevenc@voxtelecom.co.za>
2014-04-11 12:15:26 +0200

    Initial configuration * Added devise for authentication

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
 end
+gem 'devise'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 # Use unicorn as the app server
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   validates :user_name, presence: true, length: { minimum: 6 }
 end
