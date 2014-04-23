fc30fccc2f7d4bd77eb6f9b9394a82c166eea728
Steven Carter <stevenc@voxtelecom.co.za>
2014-04-23 16:16:36 +0200

    * Added in some extra profile information * Standardised the forms (Mostly) * Implemented the percentage complete information for the profile (This is still not really correct, it is missing important things like taking into account empty values correctly, eg: Date and Gender)

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -14,6 +14,6 @@ class ProfileController < ApplicationController
   private
 
   def profile_params
-    params.require(:profile).permit(:first_name, :last_name)
+    params.require(:profile).permit(:first_name, :last_name, :gender, :date_of_birth)
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,11 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  before_save do
+    self.complete = 0
+    self.attributes.each do |k, v|
+      self.complete += 1 unless k.eql?('id') || k.eql?('complete') || (v.kind_of?(String) && v.empty?)
+    end
+    self.complete = ((complete / (attributes.count - 2)) * 100).round(2)
+  end
 end
